5c0a0c892aee48c98f17e2653b5d2e13aa44a0a9
hetuw <alexandrossupertus@outlook.com>
2019-11-23 05:55:09 +0100

    dirty fix to make twin abort x key work

--- gameSource/hetuwmod.cpp
+++ gameSource/hetuwmod.cpp
@@ -2374,7 +2374,7 @@ bool HetuwMod::livingLifeKeyDown(unsigned char inASCII) {
 	if (!commandKey && isCharKey(inASCII, charKey_xRay)) {
 		if (bHoldDownTo_XRay) bxRay = true;
 		else bxRay = !bxRay;
-		return true;
+		return false; // so it doesnt block the twin abort screen or the vanilla click through feature
 	}
 	if (!commandKey && isCharKey(inASCII, charKey_TeachLanguage)) {
 		bTeachLanguage = !bTeachLanguage;
