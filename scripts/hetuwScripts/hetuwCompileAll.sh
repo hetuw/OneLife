b5b5f78bdf0756471f8a94ae13831617aa9ca889
Joey <9813078+PXshadow@users.noreply.github.com>
2023-06-05 19:12:58 +0000

    fix windows

--- scripts/hetuwScripts/hetuwCompileAll.sh
+++ scripts/hetuwScripts/hetuwCompileAll.sh
@@ -19,7 +19,7 @@ then
 	echo "compiling for windows..."
 	cd windows
 	./runToBuild 5 || { echo "Error while compiling windows"; exit 1; }
-	cp OneLifeApp ../bin/OneLifeApp_H_windows.exe
+	cp OneLifeApp.exe ../bin/OneLifeApp_H_windows.exe
 	cd ..
 fi
 
